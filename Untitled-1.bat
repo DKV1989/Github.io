de7819cd26625c1beb6c91294422261175e40e2e
DKV1989 <davidkennyvaughan@gmail.com>
2025-07-16 12:48:27 +0100

    Create Untitled-1.bat

---
+++ Untitled-1.bat
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0
+
+    <h1>David Kenny-Vaughan</h1>
+    </html>
+</head>
+<body>
+    <p>Welcome to my personal webpage!</p>
+    <p>This is a simple HTML document.</p>
+    <p>Feel free to explore and learn more about me.</p>
